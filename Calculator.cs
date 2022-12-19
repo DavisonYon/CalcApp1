e1903debe88a2c06e632e25203f4f9a78ea0656d
DavisonYon <davison.yon@gmail.com>
2022-12-19 13:24:31 -0500

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Calculator
+{
+	public Calculator()
+	{
+	}
+
+	public float Add(params float[] numbers)
+	{
+        float answer = 0;
+        foreach (float number in numbers)
+        {
+            answer += number;
+        }
+		return answer;
+	}
+    public float Subtract(params float[] numbers)
+    {
+        float answer = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            answer -= numbers[i];
+        }
+        return answer;
+    }
+
+    public float Multiply(params float[] numbers)
+    {
+        float answer = 1;
+        foreach (float number in numbers)
+        {
+            answer *= number;
+        }
+        return answer;
+    }
+
+    public float Divide(float a, float b)
+    {
+        float answer = a / b;
+        return answer;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a new Calculator object called calc
+Calculator calc = new Calculator();
+
+//Addition
+float add = calc.Add(100, 253, 75, 3); 
+Console.WriteLine(add);
+
+//Subtraction
+float subtract = calc.Subtract(100, 200);
+Console.WriteLine(subtract);
+
+//Multiplication
+float multiply = calc.Multiply(1,2,3,4,5);
+Console.WriteLine(multiply);
+
+//Division
+float divide = calc.Divide(10, 20);
+Console.WriteLine(divide);
